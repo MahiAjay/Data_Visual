638e01ed4c90200c7f3e99af0ae7fef36519a1db
Mahi <mahiajay16@gmail.com>
2023-08-26 11:51:32 +0530

    linear_regression

---
+++ ml.py
@@ -0,0 +1,15 @@
+# pandas and numpy do not relate to machine learning . It comes under data engineering. 
+# Machine learning is all about testing training data and prediction of data. Relates to human learning.
+#Many documantations and subdivisions are there in ml
+# But we will focus only on     - Supervised
+# #                                - Regression:                   ___________
+#                                    Linear regression =>         |y = mx + c| 
+#---- In linear regression we use only one feature                -----------
+#---- on which the model is performed. The data is
+#---- of two types which are dependent data and independent data.                     
+#     We basically give data which acts as the data points in the 
+#     graph and the linear regression model draws the lines in the
+#     graph. Now when we predict and analyse to the nearest line of the desired point. #         
+                                      
+# #                                - Classification
+#                               - Unsupervised  
